0755dd3d0c3ebee74e3c49317141176d84cc8e8b
Fivigati Nurhayati <fivigati@gmail.com>
2026-07-27 21:37:29 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,1 +1,42 @@
+// ======================================================
+// API NUMERA
+// ======================================================
 
+async function getClasses() {
+
+    const { data, error } = await supabase
+        .from("v_classes")
+        .select("*")
+        .order("class_name");
+
+    if (error) {
+
+        console.error(error);
+        return [];
+
+    }
+
+    return data;
+
+}
+
+
+
+async function getStudentsByClass(classId) {
+
+    const { data, error } = await supabase
+        .from("v_students")
+        .select("*")
+        .eq("class_id", classId)
+        .order("student_name");
+
+    if (error) {
+
+        console.error(error);
+        return [];
+
+    }
+
+    return data;
+
+}
