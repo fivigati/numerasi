350fe80bb991f5931e93a13576f6673f3c072e35
Fivigati Nurhayati <fivigati@gmail.com>
2026-07-27 21:34:30 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,8 @@
+// ======================================================
+// NUMERA Configuration
+// ======================================================
 
+const SUPABASE_URL = "https://vpsxtemrynbisqekbgpf.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_n_Q6JTlnckCI6vlAPu2L7A_27nkhJnB";
