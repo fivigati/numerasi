4e8a5fb220194b7e7deebe13e052ec94d3c4289e
Fivigati Nurhayati <fivigati@gmail.com>
2026-07-27 21:38:04 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,117 @@
-const kelas = document.getElementById("kelas");
-const murid = document.getElementById("murid");
+// ======================================================
+// Login Page
+// ======================================================
+
+const kelasSelect = document.getElementById("kelas");
+
+const muridSelect = document.getElementById("murid");
+
 const btnLogin = document.getElementById("btnLogin");
 
-murid.addEventListener("change", () => {
-    btnLogin.disabled = !murid.value;
+const loginForm = document.getElementById("loginForm");
+
+
+
+// ===========================================
+// LOAD KELAS
+// ===========================================
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+    const classes = await getClasses();
+
+    kelasSelect.innerHTML =
+        '<option value="">-- Pilih Kelas --</option>';
+
+    classes.forEach(kelas => {
+
+        kelasSelect.innerHTML +=
+
+        `<option value="${kelas.id}">
+            ${kelas.class_name}
+        </option>`;
+
+    });
+
+});
+
+
+
+// ===========================================
+// LOAD MURID
+// ===========================================
+
+kelasSelect.addEventListener("change", async () => {
+
+    const classId = kelasSelect.value;
+
+    muridSelect.disabled = true;
+
+    muridSelect.innerHTML =
+        '<option>Memuat...</option>';
+
+    btnLogin.disabled = true;
+
+    const students = await getStudentsByClass(classId);
+
+    muridSelect.innerHTML =
+        '<option value="">-- Pilih Nama --</option>';
+
+    students.forEach(student => {
+
+        muridSelect.innerHTML +=
+
+        `<option value="${student.id}">
+            ${student.student_name}
+        </option>`;
+
+    });
+
+    muridSelect.disabled = false;
+
+});
+
+
+
+// ===========================================
+// AKTIFKAN BUTTON
+// ===========================================
+
+muridSelect.addEventListener("change", () => {
+
+    btnLogin.disabled = muridSelect.value === "";
+
+});
+
+
+
+// ===========================================
+// LOGIN
+// ===========================================
+
+loginForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const student = {
+
+        classId: kelasSelect.value,
+
+        className:
+            kelasSelect.options[kelasSelect.selectedIndex].text,
+
+        studentId: muridSelect.value,
+
+        studentName:
+            muridSelect.options[muridSelect.selectedIndex].text
+
+    };
+
+    localStorage.setItem(
+        "numera_student",
+        JSON.stringify(student)
+    );
+
+    window.location.href = "dashboard.html";
+
 });
