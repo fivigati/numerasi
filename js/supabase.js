2039d1aa682ce4c9790d9a551299b3ea7a21fd30
Fivigati Nurhayati <fivigati@gmail.com>
2026-07-27 22:10:53 +0700

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@
 // Create Supabase Client
 // ======================================================
 
-const supabase = window.supabase.createClient(
+const sb = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
